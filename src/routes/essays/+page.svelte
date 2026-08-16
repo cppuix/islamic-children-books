f941817bdd39739ec05a093a469b1fbf91d2497a
Abderrahmene Mihoub <abderrahmene_mhb@proton.me>
2026-08-16 23:05:32 +0100

    styles blogs enteries

--- src/routes/essays/+page.svelte
+++ src/routes/essays/+page.svelte
@@ -7,23 +7,53 @@
 <h2 class="page-title">Essays</h2>
 
 <ul>
-{#each essays as essay}
-    <li>
-        <a href="{essay.link}">{essay.title}</a>
-        <p class="lead">{essay.lead}</p>
-    </li>
-{/each}
+    {#each essays as essay}
+        <li>
+            <a href={essay.link}>{essay.title}</a>
+            <p class="lead">{essay.lead}</p>
+            <p class="date">{essay.date}</p>
+            <div class="tags">
+                {#each essay.tags as tag}
+                    <a class="tag" href="">{tag}</a>
+                {/each}
+            </div>
+        </li>
+    {/each}
 </ul>
 
 <style>
-    a
+    li 
     {
+        margin-bottom: 2rem;
+    }
+    a {
         text-decoration: underline;
+        color: var(--leather-binding);
     }
 
-    .lead
-    {
+    .lead {
         color: var(--ink-faded);
         font-size: 0.8em;
+    }
+    .date {
+        color: var(--ribbon-red);
+        font-size: 0.8em;
+    }
+
+    .tags {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1em;
+    }
+    .tag {
+        text-decoration: none;
+        padding: 6px;
+        font-size: 0.4rem;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        color: var(--paper-page);
+        background-color: var(--gold-leaf);
+        border-radius: 9999px;
+        font-weight: bold;
     }
 </style>
